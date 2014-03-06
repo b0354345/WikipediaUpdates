fb029b15502e41f25b99bf37406d4f4e72fe4cde
b0354345 <e106625@hotmail.com>
2014-03-06 00:10:16 +0000

    Implemented one cassandra table to store information returned from wikipadia api

--- src/main/java/uk/ac/ncl/cs/csc8498/httpclient/JDOMXMLParser.java
+++ src/main/java/uk/ac/ncl/cs/csc8498/httpclient/JDOMXMLParser.java
@@ -2,6 +2,7 @@ package uk.ac.ncl.cs.csc8498.httpclient;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.ParseException;
 import java.util.Iterator;
 import java.util.List;
 
@@ -14,7 +15,7 @@ import org.jdom2.input.SAXBuilder;
 
 public class JDOMXMLParser {
 	private static long count;
-
+	public static CassandraCluster cluster = new CassandraCluster();
     public static void parseXML(String filename) {
     	//creating JDOM SAX parser
         SAXBuilder builder = new SAXBuilder();
@@ -36,14 +37,30 @@ public class JDOMXMLParser {
         
         List<Element> rcList = rcNode.getChildren();
         
+       
       
         //Iterating over all childs in XML
         Iterator<Element> itr = rcList.iterator();
         while(itr.hasNext()){
         	Element rc = itr.next();
-            //reading attribute from Element using JDOM
-            System.out.println("User: " + rc.getAttributeValue("user") + " Title: " + rc.getAttributeValue("title") + " Timestamp: " + rc.getAttributeValue("timestamp"));  
+        	String user = rc.getAttributeValue("user");
+        	String title = rc.getAttributeValue("title");
+        	String timestamp = rc.getAttributeValue("timestamp");
+        	String comments = rc.getAttributeValue("comment");
+        	
+        	 //reading attribute from Element using JDOM
+            System.out.println("User: " + user + " | Title: " + title + " | Timestamp: " + timestamp + " | Comment: " + comments);  
             count++;
+        	
+        	try {
+				cluster.writeWikiResults(user, timestamp, title, comments);
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
         }        
     }
     
